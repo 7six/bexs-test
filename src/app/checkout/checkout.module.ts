760dfc1199961cf8f862ca8bbb876e659e4cd745
Ivo Santos <is.ivosantos@gmail.com>
2020-05-01 15:37:55 -0300

    create step box

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CheckoutRoutingModule } from '@checkout/checkout-routing.module';
 import { PaymentsComponent } from '@checkout/payments/payments.component';
 import { SummaryComponent } from './shared/summary/summary.component';
+import { StepBoxComponent } from './shared/step-box/step-box.component';
 
 
 @NgModule({
   declarations: [
     PaymentsComponent,
-    SummaryComponent
+    SummaryComponent,
+    StepBoxComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/checkout/shared/step-box/step-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-box',
+  templateUrl: './step-box.component.html',
+  styleUrls: ['./step-box.component.scss']
+})
+export class StepBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
